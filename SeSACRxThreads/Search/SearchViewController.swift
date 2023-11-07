1e3ff7eee493158f6347e5efae749d594c903da0
limsub <limsss9076@naver.com>
2023-11-07 16:58:57 +0900

    네트워크 요청 시 drive 활용해서 중복된 콜 제한

--- SeSACRxThreads/Search/SearchViewController.swift
+++ SeSACRxThreads/Search/SearchViewController.swift
@@ -9,6 +9,8 @@ import UIKit
 import SnapKit
 import RxSwift
 import RxCocoa
+import Kingfisher
+
 
 class SearchViewController: UIViewController {
     
@@ -23,7 +25,8 @@ class SearchViewController: UIViewController {
     
     let searchBar = UISearchBar()
     
-    var data = ["a", "abcd", "abaaa", "ababab", "def", "f", "de"]
+//    var data = ["a", "abcd", "abaaa", "ababab", "def", "f", "de"]
+    var data: [AppInfo] = []
     
     lazy var items = BehaviorSubject(value: data)
     
@@ -37,7 +40,7 @@ class SearchViewController: UIViewController {
         configure()
         setSearchController()
         bind()
-        bindSearchBar()
+//        bindSearchBar()
     }
     
     func bind() {
@@ -47,8 +50,9 @@ class SearchViewController: UIViewController {
                         cellIdentifier: SearchTableViewCell.identifier,
                         cellType: SearchTableViewCell.self
         )) { (row, element, cell) in
-            cell.appNameLabel.text = element
-            cell.appIconImageView.backgroundColor = .green
+            cell.appNameLabel.text = element.trackName
+            cell.appIconImageView.kf.setImage(with: URL(string: element.artworkUrl512))
+
             
             cell.downloadButton.rx.tap
                 .subscribe(with: self) { owner , value in
@@ -58,6 +62,23 @@ class SearchViewController: UIViewController {
         }
         .disposed(by: disposeBag)
         
+        // 11/6 API 요청
+        let request = BasicAPIManager.fetchData()
+            .asDriver(onErrorJustReturn: SearchAppModel(resultCount: 0, results: []))
+        
+        request
+            .drive(with: self) { owner , value in
+                owner.items.onNext(value.results)
+            }
+            .disposed(by: disposeBag)
+        
+        request
+            .map { data in
+                "\(data.results.count) 개의 겁색 결과"
+            }
+            .drive(navigationItem.rx.title)
+            .disposed(by: disposeBag)
+        
         
         // 셀 선택 시 액션 (11/3)
         Observable.zip(tableView.rx.itemSelected, tableView.rx.modelSelected(String.self))
@@ -66,38 +87,38 @@ class SearchViewController: UIViewController {
             .disposed(by: disposeBag)
     }
     
-    func bindSearchBar() {
-        // 테이블뷰 데이터 추가
-        searchBar.rx.searchButtonClicked
-            .withLatestFrom(searchBar.rx.text.orEmpty) { _, text  in
-                // searchButtonClicked -> _
-                // searchBar.rx.text.orEmpty -> text
-                return text
-            }
-            .subscribe(with: self) { owner , text  in
-                print("서치 버튼 셀렉트 : \(text)")
-                
-                // data 배열을 따로 만들었던 이유
-                // BehaviorSubject인 items에서도 value를 꺼내올 수는 있지만, 그러면 try 구문을 사용해야 한다
-                    // items.value()
-                owner.data.insert(text, at: 0)
-                owner.items.onNext(owner.data)
-            }
-            .disposed(by: disposeBag)
-        
-        
-        // 실시간 검색
-        // 매번 계속 네트워크 통신을 하는 건 불필요 -> debounce 이용해서 커서가 멈추고 몇 초 후 실행
-        searchBar.rx.text.orEmpty
-            .debounce(RxTimeInterval.seconds(1), scheduler: MainScheduler.instance)
-            .distinctUntilChanged()
-            .subscribe(with: self) { owner , value in
-                let result = value == "" ? owner.data : owner.data.filter { $0.contains(value) }
-                owner.items.onNext(result)
-                print("실시간 검색 === \(result)")
-            }
-            .disposed(by: disposeBag)
-    }
+//    func bindSearchBar() {
+//        // 테이블뷰 데이터 추가
+//        searchBar.rx.searchButtonClicked
+//            .withLatestFrom(searchBar.rx.text.orEmpty) { _, text  in
+//                // searchButtonClicked -> _
+//                // searchBar.rx.text.orEmpty -> text
+//                return text
+//            }
+//            .subscribe(with: self) { owner , text  in
+//                print("서치 버튼 셀렉트 : \(text)")
+//
+//                // data 배열을 따로 만들었던 이유
+//                // BehaviorSubject인 items에서도 value를 꺼내올 수는 있지만, 그러면 try 구문을 사용해야 한다
+//                    // items.value()
+//                owner.data.insert(text, at: 0)
+//                owner.items.onNext(owner.data)
+//            }
+//            .disposed(by: disposeBag)
+//
+//
+//        // 실시간 검색
+//        // 매번 계속 네트워크 통신을 하는 건 불필요 -> debounce 이용해서 커서가 멈추고 몇 초 후 실행
+//        searchBar.rx.text.orEmpty
+//            .debounce(RxTimeInterval.seconds(1), scheduler: MainScheduler.instance)
+//            .distinctUntilChanged()
+//            .subscribe(with: self) { owner , value in
+//                let result = value == "" ? owner.data : owner.data.filter { $0.contains(value) }
+//                owner.items.onNext(result)
+//                print("실시간 검색 === \(result)")
+//            }
+//            .disposed(by: disposeBag)
+//    }
 
     
     private func setSearchController() {
